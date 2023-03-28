2a5bf3f85c29dee4b9628efce1da4b08fa8cd08d
Shemona Singh <singhshemona@gmail.com>
2023-03-29 00:48:07 +0200

    reading from taniyas api and set up frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import { ResponsiveCirclePacking } from '@nivo/circle-packing';
+import { generateLibTree } from '@nivo/generators'
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+  const [zoomedId, setZoomedId] = useState<string | null>(null)
+
+  useEffect(() => {
+    fetch("https://store-api-w6cy.onrender.com/bookshelf")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        console.log(json)
+        // console.log(generateLibTree())
+      });
+  }, []);
+
+  const commonProperties = {
+        width: 900,
+       height: 500,
+        data: data,
+        
+      //   {
+      //     name: "science fiction",
+      //     children: [
+      //       {
+      //           name: "dark matter",
+      //           value: 1
+      //       },
+      //       {
+      //           name: "hitchhikers guide to galaxy",
+      //           value: 1 // this needs to be provided
+      //       }, 
+      //     ]
+      // },
+      
+      padding: 2,
+      id: 'name',
+      value: 'value',
+      labelsSkipRadius: 16,
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    data ?
+      <div style={{ height: "400px", width: '600px' }}>
+        <ResponsiveCirclePacking
+            {...commonProperties}
+            enableLabels
+            labelsSkipRadius={16}
+            labelsFilter={label => label.node.height === 0}
+            labelTextColor={{
+              from: 'color',
+                modifiers: [['darker', 2]],
+          }}
+            zoomedId={zoomedId}
+            motionConfig="slow"
+            onClick={node => {
+                setZoomedId(zoomedId === node.id ? null : node.id)
+            }}
+        />
+      </div>  
+      : <p>loading...</p>
+  )
 }
 
 export default App;
